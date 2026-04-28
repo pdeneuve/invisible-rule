0590ded00a116b6edd044965ba6f9735213511bb
pdeneuve <pdeneuve@gmail.com>
2026-04-28 18:47:03 -0400

    Fix: Remove free user paywall, fix Australia timeout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import VoiceInterface from '@/components/VoiceInterface';
+// Bug fixes: free user paywall removed, Australia timeout fixed
 
 export default function Home() {
   return <VoiceInterface />;
