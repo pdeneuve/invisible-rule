15e213af589876b12f5debf1f8265371bdea946d
pdeneuve <pdeneuve@gmail.com>
2026-04-28 18:55:46 -0400

    Trigger rebuild: bug fixes v2 - free users, Australia timeout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import VoiceInterface from '@/components/VoiceInterface';
-// Bug fixes: free user paywall removed, Australia timeout fixed
+// Bug fixes: free user paywall removed, Australia timeout fixed v2
 
 export default function Home() {
   return <VoiceInterface />;
